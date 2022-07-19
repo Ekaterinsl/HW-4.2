4395af776facc24ba8aad1e9e5e3d6938c3fc271
Apelsin <Apelsin@DESKTOP-I9KKGKE>
2022-07-19 22:52:49 +0400

    Добавьте файлы проекта.

---
+++ 4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue;
+            Console.WriteLine("Введите длину последовательности");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbs = new int[n];
+            for (int i = 0; i < numbs.Length; i++)
+            {
+                Console.WriteLine("Введите число");
+                numbs[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < numbs.Length; i++)
+            {
+                if (min > numbs[i])
+                {
+                    min = numbs[i];
+                  
+                }
+                
+            }
+            Console.WriteLine($"Минимальное число {min} ");
+
+            Console.ReadKey();
+        }
+    }
+}
